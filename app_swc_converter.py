e2604bac427e6a5511122e9d0e1c50ab9e6d588a
Akhmad Khudri <khudri@binadarma.ac.id>
2025-09-20 19:18:56 +0700

    Update community links in app_swc_converter.py

--- app_swc_converter.py
+++ app_swc_converter.py
@@ -84,12 +84,12 @@ with st.sidebar:
     ### ☂ RANTAI Communities
     1. [Learn3](https://learn3.streamlit.app/)
     2. [BlockPedia](https://blockpedia.streamlit.app/)
-    3. [SmartFaith](https://smartfaith.streamlit.app/)
+    3. [Diva](https://rantai-diva.streamlit.app/)
     4. [Nexus](https://rantai-nexus.streamlit.app/)
     5. [Exploratory Data Analysis](https://rantai-exploda.streamlit.app/)
     6. [Business Intelligence](https://rantai-busi.streamlit.app/)
     7. [Predictive Modelling](https://rantai-model-predi.streamlit.app/)
-    8. [Ethic, Bias, Law, & Religion](https://rantai-ethika.streamlit.app/)
+    8. [Ethic & Bias Checker](https://rantai-ethika.streamlit.app/)
 
     ---
     #### 🙌 Dukungan & kontributor
